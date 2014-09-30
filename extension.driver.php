41f5a179d7adc1f035fc2c61da14ba46d3c12fa7
Chay <chay@randb.com.au>
2014-10-01 09:58:15 +1000

    sym 2.5

--- extension.driver.php
+++ extension.driver.php
@@ -25,7 +25,7 @@
 		 * Delegate fired to add a link to Cache Management
 		 */
 		public function fetchNavigation() {
-			$group = Administration::instance()->Author->isDeveloper() ? 'developer' : 'author';
+			$group = Symphony::Author()->isDeveloper() ? 'developer' : 'author';
 
 			
 			return array(
